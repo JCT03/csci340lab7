29ddd91e17c21c50d93332de2f02be2f5e10325f
JCT03 <119342826+JCT03@users.noreply.github.com>
2023-10-08 17:14:23 -0500

    Added validation and populated database

--- SoccerRoster/Models/Player.cs
+++ SoccerRoster/Models/Player.cs
@@ -7,9 +7,17 @@ namespace SoccerRoster.Models;
 public class Player
 {
     public int Id { get; set; }
+    [Range(0, 100)]
+    [Required]
     public int Number { get; set; }
+    [StringLength(60)]
+    [Required]
     public string? Name { get; set; }
+    [Required]
+    [StringLength(10)]
     public string? Position { get; set; }
     [Display(Name = "Home Town")]
+    [Required]
+    [StringLength(60)]
     public string? HomeTown { get; set; }
 }
